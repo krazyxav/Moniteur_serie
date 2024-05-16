44a0516780f3530f4afa516e577a5a1a4fddfa51
krazyxav <95078734+krazyxav@users.noreply.github.com>
2024-05-16 23:33:53 +0200

    Update moniteur_serie_multiline_multichart.py

--- moniteur_serie_multiline_multichart.py
+++ moniteur_serie_multiline_multichart.py
@@ -6,41 +6,43 @@ import matplotlib.animation as animation
 class AnimationPlot:
 
     def animate(self, i, dataListAcX, dataListAcY, dataListAcZ, dataListGyX, dataListGyY, dataListGyZ, ser):
-        ser.write(b'a')                                     # Transmit the char 'g' to receive the Arduino data point
-        arduinoData_string = ser.readline().decode('ascii') # Decode receive Arduino data as a formatted string
-        #print(i)                                           # 'i' is a incrementing variable based upon frames = x argument
+        ser.write(b'a')                                     # renvoi a dans le port série pour recevoir les données.
+        arduinoData_string = ser.readline().decode('ascii') # traitement des données reçues au format string
         print(arduinoData_string)
         arduinoData_array = arduinoData_string.split("|")
 
         try:
-            arduinoDataAcX_float = float(arduinoData_array[0])   # Convert to float
+            arduinoDataAcX_float = float(arduinoData_array[0])
             arduinoDataAcY_float = float(arduinoData_array[1])
             arduinoDataAcZ_float = float(arduinoData_array[2])
             arduinoDataGyX_float = float(arduinoData_array[3])
             arduinoDataGyY_float = float(arduinoData_array[4])
             arduinoDataGyZ_float = float(arduinoData_array[5])
-            dataListAcX.append(arduinoDataAcX_float)              # Add to the list holding the fixed number of points to animate
+            #ajout des valeurs dans les tableaux des différentes valeurs
+            dataListAcX.append(arduinoDataAcX_float)
             dataListAcY.append(arduinoDataAcY_float)
             dataListAcZ.append(arduinoDataAcZ_float)
             dataListGyX.append(arduinoDataGyX_float)
             dataListGyY.append(arduinoDataGyY_float)
             dataListGyZ.append(arduinoDataGyZ_float)
-
-        except:                                             # Pass if data point is bad                               
+        #Si les données sont mauvaises, empêche le traitement
+        except:                             
             pass
-
-        dataListAcX = dataListAcX[-50:]                           # Fix the list size so that the animation plot 'window' is x number of points
+        #ne tient compte que des 50 derniers points
+        dataListAcX = dataListAcX[-50:]
         dataListAcY = dataListAcY[-50:]
         dataListAcZ = dataListAcZ[-50:]
         dataListGyX = dataListGyX[-50:]
         dataListGyY = dataListGyY[-50:]
         dataListGyZ = dataListGyZ[-50:]   
 
+        #nettoyage des graphiques
         axs[0].clear()
-        axs[1].clear()                                          # Clear last data frame
-        
+        axs[1].clear()
+
+        #remise en formes des graphiques
         self.getPlotFormat()
-        axs[0].plot(dataListAcX)                                   # Plot new data frame
+        axs[0].plot(dataListAcX)
         axs[0].plot(dataListAcY)
         axs[0].plot(dataListAcZ)
         axs[0].legend(['AcX','AcY','AcZ'])
@@ -51,31 +53,27 @@ class AnimationPlot:
         
         
     def getPlotFormat(self):
-        axs[0].set_ylim([-20000, 20000])                              # Set Y axis limit of plot
-        axs[0].set_title("accelerometers")                        # Set title of figure
-        #ax[0,0].set_ylabel("Value")                              # Set title of y axis
+        axs[0].set_ylim([-20000, 20000])
+        axs[0].set_title("accelerometers")
+        #ax[0,0].set_ylabel("Value")
         axs[1].set_title("Gyroscopes")
         axs[1].set_ylim([-20000, 20000])
-
-dataListAcX = []                                           # Create empty list variable for later use
+#préparation pour réutilisation des valeurs
+dataListAcX = []
 dataListAcY = []
 dataListAcZ = []
 dataListGyX = []
 dataListGyY = []
 dataListGyZ = []
 
-                                                        
-#fig = plt.figure()                                      # Create Matplotlib plots fig is the 'higher level' plot window
-#ax = fig.subplots(2)                               # Add subplot to main fig window
 fig, axs = plt.subplots(2)
 realTimePlot = AnimationPlot()
 
-ser = serial.Serial("COM3", 9600)                       # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
-time.sleep(2)                                           # Time delay for Arduino Serial initialization 
+#préparation et attente liaison série
+ser = serial.Serial("COM3", 9600)
+time.sleep(2)
 
-                                                        # Matplotlib Animation Fuction that takes takes care of real time plot.
-                                                        # Note that 'fargs' parameter is where we pass in our dataList and Serial object. 
 ani = animation.FuncAnimation(fig, realTimePlot.animate, frames=100, fargs=(dataListAcX, dataListAcY, dataListAcZ, dataListGyX, dataListGyY, dataListGyZ, ser), interval=500) 
 
-plt.show()                                              # Keep Matplotlib plot persistent on screen until it is closed
-ser.close()                                             # Close Serial connection when plot is closed
+plt.show()
+ser.close()
